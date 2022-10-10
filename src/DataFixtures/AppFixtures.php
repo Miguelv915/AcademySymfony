4e1550f7ff7e8a47062ae390b534bcc5e867b861
Carlos Chininin <carloschininin@gmail.com>
2022-10-10 12:56:32 -0500

    update fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,29 +4,81 @@ namespace Pidia\Apps\Demo\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Pidia\Apps\Demo\Entity\Config;
+use Pidia\Apps\Demo\Entity\ConfigMenu;
+use Pidia\Apps\Demo\Entity\Menu;
 use Pidia\Apps\Demo\Entity\Usuario;
 use Pidia\Apps\Demo\Entity\UsuarioRol;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    public function __construct(private readonly UserPasswordHasherInterface $passwordHasher)
     {
     }
 
     public function load(ObjectManager $manager)
     {
-        $role = new UsuarioRol();
-        $role->setNombre('Super Administrador');
-        $role->setRol('ROLE_SUPER_ADMIN');
-        $manager->persist($role);
+        $rol = new UsuarioRol();
+        $rol->setNombre('Super Admin');
+        $rol->setRol('ROLE_SUPER_ADMIN');
+        $manager->persist($rol);
 
         $user = new Usuario();
-        $user->setUsername('admin')->setFullname('Carlos Chininin');
-        $user->setPassword($this->passwordHasher->hashPassword($user, '123456'));
-        $user->setEmail('cio@pidia.pe');
-        $user->addUsuarioRole($role);
+        $user->setFullName('Super');
+        $user->setUsername('admin');
+        $user->setEmail('admin@admin.pe');
+        $user->addUsuarioRole($rol);
+        $encodedPassword = $this->passwordHasher->hashPassword($user, '123456');
+        $user->setPassword($encodedPassword);
         $manager->persist($user);
+
+        $config = new Config();
+        $config->setAlias('default');
+        $config->setNombre('Default company');
+        $config->setPropietario($user);
+        $data = [
+            'config_index' => 'Config',
+            'menu_index' => 'Menus',
+            'usuario_index' => 'Usuarios',
+            'usuario_rol_index' => 'Usuario Roles',
+            'parametro_index' => 'Parametro',
+        ];
+
+        foreach ($data as $key => $value) {
+            $configMenu = new ConfigMenu();
+            $configMenu->setName($value);
+            $configMenu->setRoute($key);
+            $manager->persist($configMenu);
+            $config->addMenu($configMenu);
+        }
+        $manager->persist($config);
+
+        $rol->setConfig($config);
+        $rol->setPropietario($user);
+        $manager->persist($rol);
+
+        $user->setPropietario($user);
+        $user->setConfig($config);
+        $manager->persist($user);
+
+        $menuPadre = new Menu();
+        $menuPadre->setName('SEGURIDAD');
+        $menuPadre->setIcon('bi bi-gear');
+        $menuPadre->setPropietario($user);
+        $menuPadre->setConfig($config);
+        $manager->persist($menuPadre);
+
+        foreach ($data as $key => $value) {
+            $menu = new Menu();
+            $menu->setName($value);
+            $menu->setRoute($key);
+            $menu->setRanking(0);
+            $menu->setParent($menuPadre);
+            $menu->setPropietario($user);
+            $menu->setConfig($config);
+            $manager->persist($menu);
+        }
 
         $manager->flush();
     }
