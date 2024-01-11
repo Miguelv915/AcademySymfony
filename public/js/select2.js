0a1b4a5267a620d60bab71f9b3c7190c2986c1c9
Carlos Chininin <carloschininin@gmail.com>
2024-01-11 01:15:33 -0500

    update select2

--- public/js/select2.js
+++ public/js/select2.js
@@ -1,9 +1,7 @@
 let Select2 = function () {
-    // Init Select2 (with .js-select2 class)
     $('.js-select2:not(.js-select2-enabled)').each((index, element) => {
         let el = $(element);
 
-        // Add .js-select2-enabled class to tag it as activated and init it
         el.addClass('js-select2-enabled').select2({
             allowClear: true,
             placeholder: el.data('placeholder') || false,
@@ -12,3 +10,7 @@ let Select2 = function () {
         });
     });
 }
+
+$(document).ready(function() {
+    Select2();
+});
