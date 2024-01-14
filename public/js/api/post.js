07420b827b9c4db28085f8a8e4e253fe194bd5be
Carlos Chininin <carloschininin@gmail.com>
2024-01-14 00:47:21 -0500

    add telegram and apis js

---
+++ public/js/api/post.js
@@ -0,0 +1,36 @@
+async function postData(url = "", data = {}) {
+    const response = await fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        // mode: "cors", // no-cors, *cors, same-origin
+        // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        // redirect: "follow", // manual, *follow, error
+        // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+// postData(url, data)
+//     .then(result => {
+//         // Operations
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+
+
+// native
+// fetch(`https://`)
+//     .then(response => response.json())
+//     .then(result => {
+//         // Operations
+//     })
+//     .catch(error => {
+//         console.error("Error:", error);
+//     });
